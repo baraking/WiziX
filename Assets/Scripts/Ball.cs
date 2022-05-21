ea7d4e3d528b1e0dd083009920c0753b94c214b8
baraking <baraking@walla.com>
2022-05-21 16:07:22 +0300

    added an init state for balls and ballsManager

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public int size;
+    public int rounte;
+    public int xDir;
+
+    private void Awake()
+    {
+        if (xDir == 0)
+        {
+            xDir = 1;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wall")
+        {
+            xDir = -xDir;
+        }
+    }
+}
---
+++ Assets/Scripts/BallsManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallsManager : MonoBehaviour
+{
+    public List<Ball> allBalls;
+    [SerializeField] GameObject ballPrefab;
+
+    private void Start()
+    {
+        allBalls = new List<Ball>();
+        foreach(Transform child in transform)
+        {
+            if (child.GetComponent<Ball>() != null && !allBalls.Contains(child.GetComponent<Ball>()))
+            {
+                allBalls.Add(child.GetComponent<Ball>());
+            }
+        }
+    }
+
+    //runs the balls update function from here instead of using multiple update functions
+    private void Update()
+    {
+        foreach(Ball ball in allBalls)
+        {
+            UpdateBallMovement(ball);
+        }
+
+        //as a tmp testing for now
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ReduceBallSize(allBalls[0]);
+        }
+    }
+
+    //using a Sin function instead of physics bouncing, just like the original game. also, would be much more easing on the device.
+    private void UpdateBallMovement(Ball ball)
+    {
+
+    }
+
+    private void ReduceBallSize(Ball ball)
+    {
+
+    }
+}
