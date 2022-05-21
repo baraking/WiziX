84db83575563d1fcbd1969b616f1ee9bc6a2b0f9
baraking <baraking@walla.com>
2022-05-21 17:28:10 +0300

    added a very basic character movement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    InputManager inputManager;
+
+    float movementSpeed = 6;
+
+    void Start()
+    {
+        inputManager = GetComponent<InputManager>();
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * inputManager.GetHorizontalMovement() * movementSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public Joystick joystick;
+
+    float horizontalMovement;
+
+    private void Update()
+    {
+        horizontalMovement = joystick.Horizontal;
+    }
+
+    public float GetHorizontalMovement()
+    {
+        return horizontalMovement;
+    }
+}
