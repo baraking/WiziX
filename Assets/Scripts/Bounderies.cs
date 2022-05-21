97c3dabea6f4f4eb93b4ef1c353df3228b444f03
baraking <baraking@walla.com>
2022-05-21 15:58:46 +0300

    added bounderies pointers

--- Assets/Scripts/Bounderies.cs
+++ Assets/Scripts/Bounderies.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Bounderies : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject bottomBoundery;
+    [SerializeField] GameObject leftBoundery;
+    [SerializeField] GameObject rightBoundery;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //may want an awake function to change the level's layout according to the resolution or something.
 }
