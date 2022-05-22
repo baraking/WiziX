9fb38a3858438d54696e6344bed9877b350ede49
baraking <baraking@walla.com>
2022-05-22 11:25:54 +0300

    added basic main menu. need to add level progression

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnePlayerOption()
+    {
+        Debug.Log("1 player option");
+    }
+
+    public void TwoPlayeresOption()
+    {
+        Debug.Log("2 playeres option");
+    }
+
+    public void ExitGameOption()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+    #else
+        Application.Quit();
+    #endif
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
